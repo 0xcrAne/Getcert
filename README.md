6553c6e7f3ae01f0a4a4a54067ccbf4152ed2e1f
0xcrAne <mhpanc@foxmail.com>
2021-09-29 18:33:51 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Getcert
-批量获取网站SSL证书里的域名
+批量获取网站SSL证书里的域名  
+用于批量扫描漏洞，然后定位资产时，提供定位效率
+
+
+```
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -u URL, --url URL     单个地址获取
+  -f FILE, --file FILE  批量获取证书，结果输入到当前目录result.txt
+Eg: 
+    python3 getcert.py -u 127.0.0.1
+    python3 getcert.py -f ip.txt
+
+```
---
+++ getcert.py
@@ -0,0 +1,58 @@
+# python3
+# -*- coding: utf-8 -*-
+# Date: 09/09/2021
+# Author: Crane
+
+import ssl
+import OpenSSL
+import argparse
+import sys
+import os
+
+
+def usage():
+    print("Eg: \n    python3 getcert.py -u 127.0.0.1")
+    print("    python3 getcert.py -f ip.txt")
+
+
+def getcert(server, port=443):
+    try:
+        cert = ssl.get_server_certificate((server, port))
+    except Exception:
+        return None
+    if not cert:
+        return None
+    result = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+    subject = result.get_subject()
+    issued_to = subject.CN  # 只取域名，其他都不要了
+    return {"issued_to": issued_to}
+
+
+if __name__ == '__main__':
+    port = '443'
+    parser = argparse.ArgumentParser(description="获取网站证书，可以单个获取和批量获取，不要带http头和端口号，如127.0.0.1或www.xxx.com")
+    parser.add_argument('-u', '--url', type=str, help="单个地址获取")
+    parser.add_argument('-f', '--file', type=str, help="批量获取证书，结果输入到当前目录result.txt")
+    args = parser.parse_args()
+    if len(sys.argv) == 3:
+        if sys.argv[1] in ['-u', '--url']:
+            issuedDic = getcert(args.url, port)
+            if issuedDic == None:
+                print("未找到ssl信息")
+            else:
+                print(issuedDic['issued_to'])
+        elif sys.argv[1] in ['-f', '--file']:
+            if os.path.isfile(args.file) == True:
+                with open(args.file) as target:
+                    hosts = []
+                    hosts = target.read().splitlines()
+                    for host in hosts:
+                        issuedDic = getcert(host, port)
+                        if issuedDic == None:
+                            print(host + ":" + "未找到ssl信息")
+                        else:
+                            with open("result.txt", "a+") as f:
+                                f.write(host + ":" + issuedDic['issued_to'] + "\n")
+    else:
+        parser.print_help()
+        usage()
